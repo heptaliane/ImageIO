152c97b5dc487befa6dca68736b029e2a059e48a
heptaliane <heptaliane@gmail.com>
2017-02-28 21:20:42 +0900

    create canvas

---
+++ src/canvas.cpp
@@ -0,0 +1,97 @@
+#include "canvas.h"
+
+
+namespace imgio {
+
+//
+// Canvas
+//
+// 2D SimpleColor matrix like image canvas
+//
+
+const SimpleColor Canvas::except = SimpleColor();
+
+// constructor which initialized by Canvas size and color
+Canvas::Canvas (unsigned int imgWidth, unsigned int imgHeight,
+        const SimpleColor &color) :
+    width(imgWidth), height(imgHeight),
+    mat(new SimpleColor[imgWidth * imgHeight]) {
+
+    int i, j;
+
+    // set initial value to matrix
+    for ( i = 0; i < imgWidth; i++ ) {
+        for ( j = 0; j < imgHeight; j++ ) {
+            mat[i * imgHeight + j] = color;
+        }
+    }
+
+}
+
+
+// destructor
+Canvas::~Canvas() {
+
+    delete mat;
+    mat = NULL;
+
+}
+
+
+// get value from matrix
+const SimpleColor &Canvas::get(unsigned int row, unsigned int col) const {
+
+    if (isIndexInRange(row, col)) {
+        return mat[row * height + col];
+    }
+    // if index is out of range, return exceptional value
+    return except;
+
+}
+
+
+// set value to matrix
+void Canvas::set(unsigned int row, unsigned int col,
+        const SimpleColor &color) {
+
+    if (isIndexInRange(row, col)) {
+        mat[row * height + col] = color;
+    }
+
+}
+
+
+// resize matrix
+void Canvas::resize(unsigned int newWidth, unsigned int newHeight) {
+
+    // load size
+    width = newWidth;
+    height = newHeight;
+
+    // update matrix
+    delete mat;
+    mat = new SimpleColor[newWidth * newHeight];
+
+}
+
+void Canvas::copy(const Canvas &obj) {
+
+    // adjust to original size
+    resize(obj.cols(), obj.rows());
+
+    int i, j;
+
+    // copy matrix
+    for ( i = 0; i < height; i++ ) {
+        for ( j = 0; j < width; j++ ) {
+            set(i, j, obj.get(i, j));
+        }
+    }
+
+}
+
+
+
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+}
---
+++ src/canvas.h
@@ -0,0 +1,129 @@
+#ifndef CANVAS_H_
+#define CANVAS_H_
+
+#include <cstddef>
+
+
+namespace imgio {
+
+
+//
+// SimpleColor is a structure which holds only RGB values.
+// the order of the values is red blue green
+//
+
+struct SimpleColor {
+
+
+    // default constructor
+    SimpleColor () :
+        red(0), green(0), blue(0) {};
+
+
+    // initialized by RGB values
+    SimpleColor (unsigned char r,
+            unsigned char g,
+            unsigned char b) :
+        red(r), green(g), blue(b) {};
+
+
+    // copy constructor
+    SimpleColor (const SimpleColor &obj) :
+        red(obj.red), green(obj.green), blue(obj.blue) {};
+
+
+    // entity
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
+
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+
+//
+// 2D SimpleColor matrix like image canvas
+//
+
+class Canvas {
+public:
+
+
+    // default constructor
+    Canvas ():
+        width(0), height(0), mat(NULL) {};
+
+
+    // initialized by Canvas size (width and height)
+    Canvas (unsigned int imgWidth, unsigned int imgHeight) :
+        width(imgWidth),
+        height(imgHeight),
+        mat(new SimpleColor[width * height]) {};
+
+
+    // initialized by Canvas size and color
+    Canvas (unsigned int imgWidth, unsigned int imgHeight,
+            const SimpleColor &color);
+
+
+    // copy constructor
+    Canvas (const Canvas &obj);
+
+
+    // destructor
+    ~Canvas ();
+
+
+    // get value from matrix
+    const SimpleColor &get(unsigned int row, unsigned int col) const;
+
+
+    // set value to matrix
+    void set(unsigned int row, unsigned int col,
+            const SimpleColor &color);
+
+
+    // resize the matrix
+    void resize(unsigned int newWidth, unsigned int newHeight);
+
+
+    // copy matrix
+    void copy(const Canvas &obj);
+
+
+    // get matrix size
+    unsigned int rows() const {
+        return height;
+    };
+
+    unsigned int cols() const {
+        return width;
+    };
+
+
+
+private:
+
+
+    // check if the coordinate is out of range or not
+    bool isIndexInRange(unsigned int row, unsigned int col) const {
+        return ( row < height && col < width );
+    };
+
+
+    // entity
+    unsigned int width;
+    unsigned int height;
+    SimpleColor *mat;
+
+
+    // constant
+    static const SimpleColor except;
+
+};
+
+
+}
+
+#endif /* CANVAS_H_ */
