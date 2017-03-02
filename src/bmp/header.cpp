52a5edada655d57887d11a3358398878407097bc
heptaliane <heptaliane@gmail.com>
2017-03-02 17:51:04 +0900

    create bitmap header

---
+++ src/bmp/header.cpp
@@ -0,0 +1,220 @@
+#include "header.h"
+
+
+namespace imgio {
+
+
+//
+// Bmp File Header
+//
+//
+// common to all bmp types, and always locate at the begining of the file
+//
+
+
+// constance
+const unsigned int BmpFileHeader::length = 14;
+
+const unsigned char BmpFileHeader::type[2] = {
+    'B', 'M'
+};
+
+const unsigned char BmpFileHeader::reserved1[2] = {
+    static_cast<unsigned char>(0), static_cast<unsigned char>(0)
+};
+
+const unsigned char BmpFileHeader::reserved2[2] = {
+    static_cast<unsigned char>(0), static_cast<unsigned char>(0)
+};
+
+
+
+// convert data to binary bitmap file header
+void BmpFileHeader::toBinary (std::vector<unsigned char> *bin) const {
+
+    // prepare buffer
+    std::vector<unsigned char> buffer(4);
+
+    // delete container content
+    bin->clear();
+
+    // store the header data
+    bin->push_back(type[0]);
+    bin->push_back(type[1]);
+
+    conv::convert(size, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    bin->push_back(reserved1[0]);
+    bin->push_back(reserved1[1]);
+
+    bin->push_back(reserved2[0]);
+    bin->push_back(reserved2[1]);
+
+    conv::convert(offset, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+};
+
+
+// convert data to std::string
+std::string BmpFileHeader::toString () const {
+
+    std::ostringstream oss;
+
+    oss << "File size: " << size << '\n';
+    oss << "Offset size: " << offset << '\n';
+
+    return oss.str();
+};
+
+
+
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+
+
+//
+// Bitmap Core Header
+//
+// used in OS / 2 bitmap
+// it is located after file header
+//
+
+
+
+// constance
+const unsigned int BmpCoreHeader::length = 12;
+const unsigned char planes[2] = {
+    static_cast<unsigned char>(1), static_cast<unsigned char>(0)
+};
+
+
+
+// convert data to binary bitmap file header
+void BmpCoreHeader::toBinary (std::vector<unsigned char> *bin) const {
+
+    // prepare buffer
+    std::vector<unsigned char> buffer(4);
+
+    // delete container content
+    bin->clear();
+
+    // store the header data
+    conv::convert(length, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(width, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(height, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    bin->push_back(planes[0]);
+    bin->push_back(planes[1]);
+
+    conv::convert(bitCount, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+};
+
+
+
+// convert data to std::string
+std::string BmpCoreHeader::toString () const {
+
+    std::ostringstream oss;
+
+    oss << "Image size: " << width << 'x' << height << '\n';
+    oss << "bit count: " << bitCount << '\n';
+
+    return oss.str();
+};
+
+
+
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+
+
+//
+// Bitmap Infomation Header
+//
+// used in windows bitmap
+// it is located after file header
+//
+
+
+// constance
+const unsigned int BmpInfoHeader::length = 40;
+const unsigned char BmpInfoHeader::planes[2] = {
+    static_cast<unsigned char>(1), static_cast<unsigned char>(0)
+};
+
+
+
+// convert data to binary bitmap file header
+void BmpInfoHeader::toBinary (std::vector<unsigned char> *bin) const {
+
+    // prepare buffer
+    std::vector<unsigned char> buffer(4);
+
+    // delete container content
+    bin->clear();
+
+    // store the header data
+    conv::convert(length, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(width, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(height, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    bin->push_back(planes[0]);
+    bin->push_back(planes[1]);
+
+    conv::convert(bitCount, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(compression, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(sizeImage, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(xPixPerMeter, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(yPixPerMeter, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(clrUsed, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+    conv::convert(cirImportant, &buffer);
+    bin->insert(bin->end(), buffer.begin(), buffer.end());
+
+};
+
+
+// convert data to std::string
+std::string BmpInfoHeader::toString () const {
+
+    std::ostringstream oss;
+
+    oss << "Image size: " << width << 'x' << height << '\n';
+    oss << "bit count: " << bitCount << '\n';
+    oss << "compression type: " << compression << '\n';
+    oss << "image length: " << sizeImage << " bytes" << '\n';
+    oss << "pixel per meter: (" << xPixPerMeter << ',' << yPixPerMeter
+        << ")\n";
+    oss << "used palette: " << clrUsed << '\n';
+    oss << "important palette: " << cirImportant << '\n';
+
+    return oss.str();
+};
+
+
+}
---
+++ src/bmp/header.h
@@ -0,0 +1,217 @@
+#ifndef BMP_HEADER_H_
+#define BMP_HEADER_H_
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+#include "../converter.h"
+
+namespace imgio {
+
+
+//
+// Bitmap File Header
+//
+//
+// common to all bmp types, and always locate at the begining of the file
+//
+
+
+
+class BmpFileHeader {
+public:
+
+
+    // default constructor
+    BmpFileHeader () :
+        size(0), offset(0) {};
+
+
+    // initialized by file size and header + palette size
+    BmpFileHeader (unsigned int bmpSize, unsigned int bmpOffset) :
+        size(bmpSize), offset(bmpOffset) {};
+
+
+    // initialized by raw binary
+    explicit BmpFileHeader (
+            const std::vector<unsigned char> &header, int begin = 0) :
+        size(conv::dumpUInt(header, begin + 2)),
+        offset(conv::dumpUInt(header, begin + 10)) {};
+
+
+    // copy constructor
+    BmpFileHeader (const BmpFileHeader &obj) :
+        size(obj.size), offset(obj.offset) {};
+
+
+
+    // convert data to binary bitmap file header
+    void toBinary (std::vector<unsigned char> *bin) const;
+
+
+    // convert data to std::string
+    std::string toString () const;
+
+
+    // entity
+    unsigned int size;
+    unsigned int offset;
+
+
+    // constance
+    static const unsigned int length;
+    static const unsigned char type[2];
+    static const unsigned char reserved1[2];
+    static const unsigned char reserved2[2];
+
+};
+
+
+//  ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+
+//
+// Bitmap Core Header
+//
+// used in OS / 2 bitmap
+// it is located after file header
+//
+
+
+
+class BmpCoreHeader {
+public:
+
+
+    // default constructor
+    BmpCoreHeader () :
+        width(0), height(0), bitCount(24) {};
+
+
+    // initialized by image size (width and height)
+    BmpCoreHeader (short imgWidth, short imgHeight,
+            unsigned short imgBitCount = 24) :
+        width(imgWidth), height(imgHeight), bitCount(imgBitCount) {};
+
+
+    // initialized by raw binary
+    explicit BmpCoreHeader (
+            const std::vector<unsigned char> &header, int begin = 0) :
+        width(conv::dumpShort(header, begin + 2)),
+        height(conv::dumpShort(header, begin + 4)),
+        bitCount(conv::dumpUShort(header, begin + 10)) {};
+
+
+    // copy constructor
+    BmpCoreHeader (const BmpCoreHeader &obj) :
+        width(obj.width), height(obj.height), bitCount(obj.bitCount) {};
+
+
+
+    // convert data to binary bitmap file header
+    void toBinary (std::vector<unsigned char> *bin) const;
+
+
+    // convert data to std::string
+    std::string toString () const;
+
+
+    // entity
+    short width;
+    short height;
+    unsigned short bitCount;
+
+
+    // constance
+    static const unsigned int length;
+    static const unsigned char planes[2];
+
+};
+
+
+//  ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+
+//
+// Bitmap Infomation Header
+//
+// used in windows bitmap
+// it is located after file header
+//
+
+
+
+class BmpInfoHeader {
+public:
+
+
+    // default constructor
+    BmpInfoHeader () :
+        width(0), height(0), bitCount(0),
+        compression(0), sizeImage(0), xPixPerMeter(0), yPixPerMeter(0),
+        clrUsed(0), cirImportant(0) {};
+
+
+    // initialized by image size (width and height)
+    BmpInfoHeader (int imgWidth, int imgHeight,
+            unsigned short imgBitCount = 24) :
+        width(imgWidth), height(imgHeight), bitCount(imgBitCount),
+        compression(0), sizeImage(0), xPixPerMeter(0), yPixPerMeter(0),
+        clrUsed(0), cirImportant(0) {};
+
+
+    // initialized by raw binary
+    explicit BmpInfoHeader (
+            const std::vector<unsigned char> &header, int begin = 0) :
+        width(conv::dumpInt(header, begin + 4)),
+        height(conv::dumpInt(header, begin + 8)),
+        bitCount(conv::dumpUShort(header, begin + 14)),
+        compression(conv::dumpUInt(header, begin + 16)),
+        sizeImage(conv::dumpUInt(header, begin + 20)),
+        xPixPerMeter(conv::dumpInt(header, begin + 24)),
+        yPixPerMeter(conv::dumpInt(header, begin + 28)),
+        clrUsed(conv::dumpUInt(header, 32)),
+        cirImportant(conv::dumpUInt(header, 36)) {};
+
+
+
+    // copy constructor
+    BmpInfoHeader (const BmpInfoHeader &obj) :
+        width(obj.width), height(obj.height), bitCount(obj.bitCount),
+        compression(obj.compression), sizeImage(obj.sizeImage),
+        xPixPerMeter(obj.xPixPerMeter), yPixPerMeter(obj.yPixPerMeter),
+        clrUsed(obj.clrUsed), cirImportant(obj.cirImportant) {};
+
+
+
+    // convert data to binary bitmap file header
+    void toBinary (std::vector<unsigned char> *bin) const;
+
+
+    // convert data to std::string
+    std::string toString () const;
+
+
+    // entity
+    int width;
+    int height;
+    unsigned short bitCount;
+    unsigned int compression;
+    unsigned int sizeImage;
+    int xPixPerMeter;
+    int yPixPerMeter;
+    unsigned int clrUsed;
+    unsigned int cirImportant;
+
+
+    // constance
+    static const unsigned int length;
+    static const unsigned char planes[2];
+
+};
+
+
+}
+
+#endif /* BMP_HEADER_H_ */
